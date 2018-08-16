34517ed4d6d0088febdada8621b3a427fcc9dac7
李国昌 <liguochangli@gmail.com>
2018-08-17 01:58:33 +0800

    renew

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+修改自大佬的脚本
+原脚本由于api显示每个username只能爬第一页图片，修改为爬去所有图片
+修改获取照片尺寸由520变为1280
+增加超时检测，tumbler国内访问容易超时卡死
+    默认为30s，网络环境好可在34行减少数值，反之亦然
+修改只获取图片，小视频质量一般哈，有需求去爬视频网站好了
+    需要获取小视频在16行删除type=photo&即可
+没有验证结束，出现报错即完成
+使用方式不变C:\Users\liguo\Documents\GitHub\tumblr_spider
 # python3 tumblr多线程爬虫
 给定 tumblr 用户的 username ，下载图片以及视频资源。  
 声明一下:这是一个正经的爬虫，所爬取的资源跟你填入的 username 有关系，请勿随意开车。  
@@ -13,3 +22,5 @@
 # import
 >import tumblr  
 >tumblr.tumblr_id('username1,username2,username3...')
+原脚本地址
+https://github.com/yl-L/tumblr_spider
--- tumblr.py
+++ tumblr.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-import time, re, os, urllib.request,sys
+import time, re, os, urllib.request,sys,socket
 from threading import Lock, current_thread, Thread
 
 def main():
@@ -10,25 +10,36 @@ def main():
     tumblr_id(username)
 def download(args):
     print('At',time.ctime(),'开始下载%s'%args)
-    url = 'http://%s.tumblr.com/api/read/json?start=0&num=200' %args
-    req = urllib.request.Request(url)
-    req.add_header('User-Agent','Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Mobile Safari/537.36')
-    data=urllib.request.urlopen(req).read().decode('UTF-8')
-    print('%s start at '%args ,time.ctime())
-    img = r'photo-url-500":"(.{80,120}500.jpg)'#正则表达式匹配图片
-    video = r'source src=\\"(.{80,130})" type.*video-player-500'#正则表达式匹配图片
-    l = 0
-    for i in re.findall(img,data):#图片下载
-        l += 1
-        if l >100:#判断下载100张图片跳出循环
-            break
-        t = i.replace('\\', '')#替换转义字符
-        imgfilename = t.split("/")[-1].replace('_500','').replace('_r1','').replace('tumblr','%s'%args)#分割并修改保存的文件名
-        print('At',time.ctime(),'Downloadiing %s from userID %s' % (imgfilename, args))
-        imgDir = "./etc/img/"
-        if not os.path.exists(imgDir):#判断路径是否存在
-            os.makedirs(imgDir)
-        urllib.request.urlretrieve(t, "%s%s" %(imgDir,imgfilename))
+    begin = 0 #start数值
+    end = 50  #number数值
+    while begin < 10000:
+        url = 'http://%s.tumblr.com/api/read/json?type=photo&start=%u&num=%u' %(args,begin,end)
+        req = urllib.request.Request(url)
+        req.add_header('User-Agent','Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Mobile Safari/537.36')
+        data=urllib.request.urlopen(req).read().decode('UTF-8')
+        #print (data) #调试使用
+        print('%s start at '%args ,time.ctime())
+        img = r'photo-url-1280":"(.{80,120}1280.jpg)'#正则表达式匹配图片
+        video = r'source src=\\"(.{80,130})" type.*video-player-500'#正则表达式匹配图片
+        l = 0
+        #print(re.findall(img,data)) #调试使用
+        for i in re.findall(img,data):#图片下载
+            l += 1
+            t = i.replace('\\', '')#替换转义字符
+            imgfilename = t.split("/")[-1].replace('_1280','').replace('_r1','').replace('tumblr','%s'%args)#分割并修改保存的文件名
+            print('At',time.ctime(),'Downloading %s from userID %s' % (imgfilename, args))
+            imgDir = "./etc/img/"
+            if not os.path.exists(imgDir):#判断路径是否存在
+                os.makedirs(imgDir)
+            socket.setdefaulttimeout(30)
+            try:
+                gg = urllib.request.urlretrieve(t, "%s%s" %(imgDir,imgfilename))
+            except socket.timeout:
+                print("下载超时")
+                continue
+            #print (gg) #调试使用
+        begin += 50
+        end += 50
     print('图片已完成下载%s' %args)
 
     for v in re.findall(video,data):#视频下载
